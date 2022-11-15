2d8fa7ca0185911d9b3617bcbd0f7c99dbb0ed24
William Huang <hh921088586@berkeley.edu>
2022-11-15 00:32:13 -0800

    added route code

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,17 +1,49 @@
 let todoData =  require("../data/todoData")
 
-module.exports = function(app) {
+const fs = require('fs');
 
-    app.get("/api/notes", function(req, res) {
-        res.json(todoData);
+module.exports = (app) => {
+
+    app.get("/api/notes", (req, res) => {
+        fs.readFile("db/db.json","utf-8", (error, data) =>{
+            if (error) {
+                return console.log(error);
+            }
+            console.log(data);
+            res.json(JSON.parse(data))
+        });
+        // res.json(todoData);
       });
 
-    app.post("/api/notes", function(req, res) {
-        todoData.push(req.body);
-        res.json(true);});
+    app.post("/api/notes", (req, res) => {
+        console.log(req.body);
 
-    app.post("/api/clear", function(req, res) {
-        todoData.length = 0;
-        res.json({ ok: true });
-});
+        let notTask = {
+            title: req.title,
+            text: req.text,
+            id: req.id
+        }
+
+        res.send(JSON.stringify(req.body));
+
+        data.push(req.body);
+        renderHtml();
+    });
+
+    // app.post("/api/notes", function(req, res) {
+    //     todoData.push(req.body);
+    //     res.json(true);});
+
+    app.delete("/api/notes/:id", (req, res) => {
+        res.send("Delete request at notes")
+    });
+
+//     app.post("/api/clear", function(req, res) {
+//         todoData.length = 0;
+//         res.json({ ok: true });
+// });
+};
+
+function renderHtml() {
+    fs.writeFileSync("./public/assets/notes.html", render(data), "utf-8")
 }
--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,17 @@
-let express = require('express');
+const express = require('express');
 let http = require ('http');
 let fs = require ('fs');
-let path = require ('path');
-let app = express();
-let PORT = 8080;
+const path = require ('path');
+const app = express();
+const PORT = 8080;
 
-let server = http.createServer();
+// let server = http.createServer();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => console.log(`Server is listening on: http://localhost: ${PORT}`));
