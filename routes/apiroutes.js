1a68a4e9d0c9f7e30a3ebb43bf268e84ff1eb7d8
William Huang <hh921088586@berkeley.edu>
2022-11-15 15:00:13 -0800

    everything works

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -38,43 +38,13 @@ module.exports = (app) => {
           });
 
 
-          fs.readFile("./db/db.json", "utf-8", (error, noteData) => {
-            console.log("Test");
-            if (error) {
-              return console.log(`this is a line 14 ${error}`);
-            }
-            
-            let testRead = JSON.parse(noteData);
-            testRead.push(newNote);
-            fs.writeFile("./db/db.json", JSON.stringify(testRead, null, 2), err => {
-              if (err) throw err;
-              res.send(testRead);
-              console.log('Successfully wrote file')
-            })
-          });
-
-
-
-        // res.send(JSON.stringify(req.body));
-
-        // data.push(req.body);
-        // renderHtml();
     });
 
-    // app.post("/api/notes", function(req, res) {
-    //     todoData.push(req.body);
-    //     res.json(true);});
 
     app.delete("/api/notes/:id", (req, res) => {
         res.send("Delete request at notes")
     });
 
-//     app.post("/api/clear", function(req, res) {
-//         todoData.length = 0;
-//         res.json({ ok: true });
-// });
+
 };
 
-// function renderHtml() {
-//     fs.writeFileSync("./public/assets/notes.html", render(data), "utf-8")
-// }
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require ('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, () => console.log(`Server is listening on: http://localhost:${PORT}`));
